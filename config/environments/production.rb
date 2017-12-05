9b2bf0440bb555c6dcf12f77125a9cf507688155
Vincent Tsang <vstsang@gmail.com>
2017-12-05 17:32:42 +0800

    Configure figaro and sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set URL to localhost for production environment
+  config.action_mailer.default_url_options = { host: 'glacial-fjord-84001.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 

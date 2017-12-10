263682520f06541bd87ff0b658b8f932172d4e19
Vincent Tsang <vstsang@gmail.com>
2017-12-10 12:27:09 +0800

    Timeslot order start_time by ASC

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,6 @@
 class Timeslot < ActiveRecord::Base
   belongs_to :booking
   belongs_to :tradesman, :class_name => 'User', :foreign_key => 'tradesman_id'
+
+  default_scope { order('start_time ASC') }
 end
